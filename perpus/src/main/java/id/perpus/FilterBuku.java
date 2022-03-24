d062fcbfc49efbdcddf97658b505284e5bd6855d
miskiyahr <97836792+miskiyahr@users.noreply.github.com>
2022-03-24 18:38:24 +0700

    Update FilterBuku.java

--- perpus/src/main/java/id/perpus/FilterBuku.java
+++ perpus/src/main/java/id/perpus/FilterBuku.java
@@ -1,5 +1,11 @@
 package id.perpus;
 
-public class FilterBuku {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
+public interface FilterBuku {
+  boolean apply(Buku b);
 }
+
