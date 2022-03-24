245a1d2e3e6aba43f3d56a7f14a6c9066b91a5c4
miskiyahr <97836792+miskiyahr@users.noreply.github.com>
2022-03-24 18:41:55 +0700

    Tambah class FilterTahunTerbit di class FilterBuku

--- perpus/src/main/java/id/perpus/FilterBuku.java
+++ perpus/src/main/java/id/perpus/FilterBuku.java
@@ -9,3 +9,13 @@ public interface FilterBuku {
   boolean apply(Buku b);
 }
 
+class FilterTahunTerbit implements FilterBuku {
+  private Function<LocalDate, Boolean> comparison;
+
+  static FilterTahunTerbit After(int tahun) {
+    final LocalDate tanggal = LocalDate.of(tahun, 12, 31);
+    FilterTahunTerbit filter = new FilterTahunTerbit();
+    filter.comparison = tanggal::isBefore;
+    return filter;
+  }
+
