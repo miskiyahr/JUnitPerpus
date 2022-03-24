393dd2a387aa138d46758c53f8f292cca253eaf4
lisyaaprl <82822249+lisyaaprl@users.noreply.github.com>
2022-03-24 12:09:36 +0700

    Update Buku.java

--- perpus/src/main/java/id/perpus/Buku.java
+++ perpus/src/main/java/id/perpus/Buku.java
@@ -1,5 +1,17 @@
 package id.perpus;
 
-public class Buku {
+import java.time.LocalDate;
 
+public class Buku implements Comparable<Buku> {
+    private final String judul;
+    private final String pengarang;
+    private final LocalDate tanggalTerbit;
+    private LocalDate mulaiBaca;
+    private LocalDate selesaiBaca;
+
+    public Buku(String judul, String pengarang, LocalDate tanggalTerbit) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.tanggalTerbit = tanggalTerbit;
+    }
 }
